27bb380a2a53087075c037724c5506b76241f61f
NanaDuku <wandohkesson@gmail.com>
2020-07-19 19:47:52 +0000

    something on autoload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+require "vendor/autoload.php";
+
+use Vehicle\Car;
+
+class MotorBike extends Car
+{
+    //inherit all properties and methods of car since they are the same things i want to use
+
+}
+
+$m1 = new MotorBike("susuki","2007","automatic");
+echo $m1->getBrand();
